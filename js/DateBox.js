cec5aa9590d2a569527f68ae86603e02a90ab01d
zino1187 <zino1187@gmail.com>
2021-03-30 16:38:33 +0900

    c

--- js/DateBox.js
+++ js/DateBox.js
@@ -3,18 +3,19 @@
 일정 등록 및 날짜 표가 가능한 객체
 */
 class DateBox{
-    constructor(container, width, height){
+    constructor(container, width, height, text, bg){
         this.container=container;
         this.div=document.createElement("div");
         this.width=width;
         this.height=height;
-
+        this.div.innerText=text; //인수로 넘겨받은 문자열을 div에 출력
+        this.bg=bg;//배경색을 인수로 넘겨받자!!
         this.init();
     }    
     init(){
         this.div.style.width=this.width+"px";        
         this.div.style.height=this.height+"px"; 
-        this.div.style.background="yellow";
+        this.div.style.background=this.bg;
         this.div.style.border="1px solid red";
         this.div.style.boxSizing="border-box";
         this.div.style.float="left"; //block 이니깐 
